f05a1b1c4cd923468280d68b79369bc17f4575ec
AbhinavJosephDharani <abhinav.dharani7@gmail.com>
2025-12-17 13:36:38 -0500

    Initialize React frontend with Vite scaffold

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Campus Navigation App</h1>
+      <p>Welcome to the Campus Navigation App!</p>
+    </div>
+  );
+}
+
+export default App;
